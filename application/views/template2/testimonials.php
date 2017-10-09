395f463be88d8deb14de5cae40e5bb483a200d28
amitambekar <amit.ambekar979@gmail.com>
2017-10-09 23:56:05 +0530

    changes for testimonials

--- application/views/template2/testimonials.php
+++ application/views/template2/testimonials.php
@@ -12,20 +12,150 @@
         <div class="auto-container">
 
             <div class="row clearfix">
-                <article class="col-md-4 col-sm-6 col-xs-12">
+                <article class="col-md-12 col-sm-12 col-xs-12">
                     <div class="testimonial-item">
                         <div class="content">
                             <span class="fa fa-quote-left"></span>
-                            <p>Till yet good experience, good profit, thanks to Amit sir. Option Mastery must do everyone, in option mastery till yet 100% profit, no loss.</p>
+                            <p>Good experience, I have learned from zero level, getting personal touch from sir. Got something new compare to any other things. Some outside people avoid to share knowledge but here I got all knowledge, I cannot compare it. Now it depends upon us how much we can learn and earn.As sir said about observation, its matter a lot here. If we follow this no person can be left behind.</p>
                         </div>
                         <div class="author">
                             <ul class="list-inline">
                                 <li>
-                                    <img src="<?= imagePath('assets/template2/images/team/5.jpg','',0,0); ?>" alt="">
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/rajeshcivil.jpg','',0,0); ?>" alt="">
                                 </li>
                                 <li>
-                                    <h5>Bikrant Dutt</h5>
-                                    <p>India</p>
+                                    <h5>Rajesh Mishra</h5>
+                                    <p>Civil Engineer</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>My confidence level boost up from how to do analysis in option writing with different parameters. I think it’s good as sir said average 5 to 10 percent return per month but it’s possible to earn more. Now we know how we can do successfully work in option.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/milind.png','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>Milind Raut</h5>
+                                    <p>IT Engineer</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>My name is Samita Rane. I like two things. Low risk and no need of more time, thanks sir. If you have exact knowledge of stock analysis, we can earn good money from stock trading.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/samita.jpg','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>Samita Rane</h5>
+                                    <p>Housewife And Insurance Agent</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>Online trading institute has been essential in providing me the start that i needed in my trading career. I got the support and guidance from Amit sir that everyone needed to overcome the initial fear of losing money in financial markets. Amit sir could be your edge in the financial market when it comes to mentorship and experience.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/amitsingh.jpg','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>Amit Singh</h5>
+                                    <p>BFM Student</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>Hello all, after earnings technical chart trading I am able to earn 1% profit on capital average everyday. Thanks to Sir.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/mehul.png','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>MEHUL</h5>
+                                    <p>Civil Engineer</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>To the world you may be just a teacher but to your student's you are a HERO - AMIT SIR. Being the first student of this Online Trading Institute. It gives me immense pleasure to say- Amit Sir has always been a source of inspiration. Never Say die attitude - always exploring much newer things and always helping there students as and when possible.The way I look into my life by joining Online Trading Institute is giving me a lot more information about the Stock Market than just numbers. Amit Sir's Online Trading Institute has always been a source of Inspiration - the knowledge regarding the fundamentals and technical analysis, chart readings work wonders. It multiples our finances and makes a usually complicated market easy to trade with. Showing us a way to financial freedom.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/deepa.jpg','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>Deepa Modgekar</h5>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>If you want to become financial success in your life, you have to learn. Amit sir teach us short and simple intra-day techniques so I am able to earn now. If I can do it, you can also do it by joining online trading institute. Thank you sir.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/mulchand.JPG','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>Mulchand Barai</h5>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>     
+                </article>
+                <article class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="testimonial-item">
+                        <div class="content">
+                            <span class="fa fa-quote-left"></span>
+                            <p>I know Mr Amit Jain (MBA in Finance) since last one & half year. He runs online trading classes & other classes also. He is Hardworking, Honest & Sincere. He believes in financial freedom. He believes in distributing his knowledge for financial growth of self as well as other person attached with him. I & my friends have invested money with him for growth of our fund. I am proud to say that we all are getting good returns from him in time to time & sometimes before time. Money invested with him is safe.</p>
+                        </div>
+                        <div class="author">
+                            <ul class="list-inline">
+                                <li>
+                                    <img src="<?= imagePath('assets/template2/aajfxcoin/suprita.jpg','',0,0); ?>" alt="">
+                                </li>
+                                <li>
+                                    <h5>Suprita Parashare</h5>
                                 </li>
                             </ul>
                         </div>
